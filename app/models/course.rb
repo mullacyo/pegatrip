6623eb03f58952453f87b428081dab8c80666e5c
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-30 09:53:13 +0800

    Updated Course and Trip models to reflect many-to-many relationship

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+	has_many :courses_trips
+	has_many :trips, through: :courses_trips
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ApplicationRecord
+	belongs_to :user
+	has_many :courses_trips
+	has_many :courses, through: :courses_trips
 end
