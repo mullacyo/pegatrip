1c47572b0572eb7bba225a0835cba8d797ab111b
alexander <alexbaggio315@hotmail.com>
2019-01-31 12:21:29 +0800

    finished google auth

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
 
+get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+
   
 end
 
