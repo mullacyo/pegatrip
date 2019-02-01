d8ace1a0d8b869a840e395935f198c8b743eac83
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-01 11:28:30 +0800

    Laid out skeleton of code for enabling flexible search queries to enter get request

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -5,21 +5,27 @@ class YelpController < ApplicationController
   require "httparty"
   require "optparse"
   
-  def show
-  	holder = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get('https://api.yelp.com/v3/businesses/search?term=delis&location=nyc')
-  	@response = holder.parse['businesses']
-  	puts @response
+  def show  	
+  	# holder = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get('https://api.yelp.com/v3/businesses/search?term=delis&location=nyc')
+  	# @response = holder.parse['businesses']
+  	# puts @response
   end
 
   def index
-  	response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get('https://api.yelp.com/v3/businesses/search?term=delis&location=nyc')
+  	#Change to make a response flexible from a query method
+  	response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get('https://api.yelp.com/v3/businesses/search?term=coffee&location=nyc')
   	@yelps = response.parse['businesses']
+  	# @yelps.each do |yelp|
+  	# 	Yelp.create(yelp_reference: yelp['id'])
+  	# end
   end
 
   def map
+  	#??? Shows results on a map? To be implemented
   end
 
   def search
+  	#Shows the search form
   end
 
 end
--- app/models/yelp.rb
+++ app/models/yelp.rb
@@ -1,3 +1,26 @@
 class Yelp < Action
+	
 	include HTTParty
+
+	API_HOST = "https://api.yelp.com"
+	SEARCH_PATH = "/v3/businesses/search"
+	BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+
+	DEFAULT_BUSINESS_ID = "yelp-san-francisco"
+	DEFAULT_TERM = "dinner"
+	DEFAULT_LOCATION = "San Francisco, CA"
+	SEARCH_LIMIT = 5	
+
+	def search(term, location)
+	  url = "#{API_HOST}#{SEARCH_PATH}"
+	  params = {
+	    term: term,
+	    location: location,
+	    limit: SEARCH_LIMIT
+	  }
+
+	  response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get(url, params: params)
+	  response.parse
+	end	
+
 end
