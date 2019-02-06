6e4f9783dfff82525207d592235e3887d568ea1c
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-06 12:45:01 +0800

    Removed unneccesary Yelp subclass from Action class

--- app/models/action.rb
+++ app/models/action.rb
@@ -11,30 +11,3 @@ end
 
 class Activity < Action
 end
-
-class Yelp < Action
-
-	include HTTParty
-
-	API_HOST = "https://api.yelp.com"
-	SEARCH_PATH = "/v3/businesses/search"
-	BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-
-	DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-	DEFAULT_TERM = "dinner"
-	DEFAULT_LOCATION = "San Francisco, CA"
-	SEARCH_LIMIT = 5	
-
-	def search(term, location)
-	  url = "#{API_HOST}#{SEARCH_PATH}"
-	  params = {
-	    term: term,
-	    location: location,
-	    limit: SEARCH_LIMIT
-	  }
-
-	  response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get(url, params: params)
-	  return response.parse
-	end	
-
-end
