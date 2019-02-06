ea88ac29473a06d95664ec2a6bd8b365676a80aa
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-06 13:52:34 +0800

    Removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "landing_page#index"
 
-  get 'yelp/index'
-  get 'yelp/map'
   get 'yelp/search'
 
   # resources :yelp
