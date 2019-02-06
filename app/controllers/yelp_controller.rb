84fc04949133e6725c1970c09b01b8be715159ea
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-06 12:24:40 +0800

    Info about businesses from Yelp saved to database after a search is run

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -7,14 +7,6 @@ class YelpController < ApplicationController
   # API_HOST = "https://api.yelp.com"
   # SEARCH_PATH = "/v3/businesses/search"
 
-
-
-  def show  	
-  	# holder = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get('https://api.yelp.com/v3/businesses/search?term=delis&location=nyc')
-  	# @response = holder.parse['businesses']
-  	# puts @response
-  end
-
   def index
   	#Change to make a response flexible from a query method
   	response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get('https://api.yelp.com/v3/businesses/search?term=coffee&location=nyc')
@@ -37,6 +29,17 @@ class YelpController < ApplicationController
 	  }
   		response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get("https://api.yelp.com/v3/businesses/search", params: search_parameters)
   		@yelps = response.parse['businesses']
+  		
+  		# Save each yelp result to the database as an action
+  		@yelps.each do |y|
+  			# Check if the yelp result was saved before
+  			if Action.find_by(api_reference: y['id']).present?
+ 				#Skip
+ 			else
+	  			# If new, make a new entry
+  				Action.create(price: y['price'].length, title: y['name'], type: "Food", description: "No description is available for this item yet", location: y['location'], api_reference: y['id'], api_source: "Yelp")
+ 			end
+  		end
   	end
 
   end
