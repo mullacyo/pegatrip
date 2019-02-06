3da308676411732a4c4722831ae7b246cf835155
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-06 17:12:12 +0800

    Basic code to extract data into command line works

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,13 +1,25 @@
 require 'HTTParty'
 require 'Nokogiri'
 
-attr_accessor :parse_page
 
-class Scaper
+class Scraper
+
+attr_accessor :parse_page
 
 	def initialize
 		doc = HTTParty.get("https://www.switchup.org/rankings/best-bootcamps-atlanta")
 		@parse_page ||= Nokogiri::HTML(doc)
+	end
+
+	def get_names
+		parse_page.css("ul").css("li").css(".ranking-item").css("h3").css("a").map {|name| name.text}.compact
+	end
+
+	scraper = Scraper.new
+	names = scraper.get_names
+
+	(0..names.size-1).each do |index| 
+		puts "##{index+1}: #{names[index]}"
 	end
 
 end
