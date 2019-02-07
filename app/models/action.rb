d0ba28a748333e4760a44f61ad6ce7e45d9e3664
Callum Tan <mullacyo@hotmail.com>
2019-02-07 15:44:44 +0800

    modified slightly so every card will show (and won't throw an error due to insufficient data)

--- app/models/action.rb
+++ app/models/action.rb
@@ -18,12 +18,8 @@ class Sightseeing < Action
 end
 
 class Activity < Action
-<<<<<<< HEAD
-end
-=======
 	def self.random
 		random_number = rand(0 .. Activity.all.length-1)
 		Activity.all[random_number]
 	end
 end
->>>>>>> added cards to the trip
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
  
-<<<<<<< HEAD
- provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']#, image_size: 'large'
-=======
  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']#, scope: ‘public_profile’
->>>>>>> added cards to the trip
 
 end
