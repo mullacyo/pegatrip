a39499b8e3c5e3537e22b21b68516932fd9e893c
farahriz <41633573+farahriz@users.noreply.github.com>
2019-02-09 15:19:39 +0800

    Ticketmaster scraping experimentation

--- app/models/switchup_scraper.rb
+++ app/models/switchup_scraper.rb
@@ -7,41 +7,43 @@ class SwitchupScraper
 attr_accessor :parse_page
 
 	def initialize(url)
-		doc = HTTParty.get(url) #only works with www.switchup.org/rankings/best-bootcamps-[location_name] for the time being
+		doc = HTTParty.get(url) 
 		@parse_page ||= Nokogiri::HTML(doc)
 	end
 
-	def get_names
-		parse_page.css("ul").css("li").css(".ranking-item").css("h3").css("a").map {|name| name.text}.compact
-	end
+	#only works with www.switchup.org/rankings/best-bootcamps-[location_name] for the time being
+		def get_names
+			parse_page.css("ul").css("li").css(".ranking-item").css("h3").css("a").map {|name| name.text}.compact
+		end
 
-	def get_ratings
-		item_header.css(".ranking-item__rating").css("a").css("span").map {|name| name.text.strip[0..-3]}.compact
-	end
+		def get_ratings
+			item_header.css(".ranking-item__rating").css("a").css("span").map {|name| name.text.strip[0..-3]}.compact
+		end
 
-	def get_prices
-		item_header.css(".ranking-item__price")
-	end
+		def get_prices
+			item_header.css(".ranking-item__price")
+		end
 
-	#helper to DRY up code
-	def item_header
-		parse_page.css("ul").css("li").css(".ranking-item").css(".ranking-item__header").css(".ranking-item__header--left")
-	end
+		def get_subjects
+			parse_page.css("table").css(".ranking-item__info-table").css("tbody")
+		end
 
-	scraper = SwitchupScraper.new("https://www.switchup.org/rankings/best-bootcamps-austin")
-	names = scraper.get_names
-	ratings = scraper.get_ratings
-	prices = scraper.get_prices
+	private
+		def item_header
+			parse_page.css("ul").css("li").css(".ranking-item").css(".ranking-item__header").css(".ranking-item__header--left")
+		end
 
-	# (0..names.size-1).each do |index| 
-	# 	puts "##{index+1}: #{names[index]} || #{ratings[index]}"
-	# end	
+		scraper = SwitchupScraper.new("https://www.switchup.org/rankings/best-bootcamps-boston")
+		names = scraper.get_names
+		ratings = scraper.get_ratings
+		prices = scraper.get_prices
+		subjects = scraper.get_subjects
 
-	(0..names.size-1).each do |index| 
-		puts "##{index+1}: #{names[index]} || #{ratings[index]} ||#{prices[index].to_s.gsub('dollar-sign--filled').count}"
-	end	
+		(0..names.size-1).each do |index| 
+			puts "##{index+1}: #{names[index]} || #{ratings[index]} ||#{prices[index].to_s.gsub('dollar-sign--filled').count} || "
+		end	
 
-
+		# pp subjects[0].children
 
 
 end
---
+++ app/models/ticketmaster_scraper.rb
@@ -0,0 +1,29 @@
+require 'HTTParty'
+require 'Nokogiri'
+
+
+class TicketmasterScraper
+
+attr_accessor :parse_page
+
+	def initialize(url)
+		doc = HTTParty.get(url) 
+		@parse_page ||= Nokogiri::HTML(doc)
+	end
+
+	def get_subjects
+		parse_page.css("table") 
+	end
+
+	private
+		scraper = TicketmasterScraper.new("https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/")
+		pp scraper.get_subjects
+
+		# (0..names.size-1).each do |index| 
+		# 	puts "##{index+1}: #{names[index]} || #{ratings[index]} ||#{prices[index].to_s.gsub('dollar-sign--filled').count} || "
+		# end	
+
+		# pp subjects[0].children
+
+
+end
